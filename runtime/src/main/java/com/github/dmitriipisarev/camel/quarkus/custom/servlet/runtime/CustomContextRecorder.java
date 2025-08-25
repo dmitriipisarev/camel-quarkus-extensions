a3a1cf783a29554e690b5966120f81c324296be4
Dmitrii Pisarev <dmitrii.pisarev.qubership@gmail.com>
2025-08-25 14:59:07 +0300

    fixes

--- runtime/src/main/java/com/github/dmitriipisarev/camel/quarkus/custom/servlet/runtime/CustomContextRecorder.java
+++ runtime/src/main/java/com/github/dmitriipisarev/camel/quarkus/custom/servlet/runtime/CustomContextRecorder.java
@@ -3,13 +3,18 @@ package com.github.dmitriipisarev.camel.quarkus.custom.servlet.runtime;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ExtendedCamelContext;
 import org.apache.camel.dsl.xml.io.XmlRoutesBuilderLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.annotations.Recorder;
 
 @Recorder
 public class CustomContextRecorder {
+    private static final Logger LOG = LoggerFactory.getLogger(CustomContextRecorder.class);
+
     public void replaceXmlBuilder(RuntimeValue<CamelContext> camelContext) {
-        ((ExtendedCamelContext) camelContext).addContextPlugin(XmlRoutesBuilderLoader.class, null);
+        LOG.info("Trying to replace xml loader");
+        ((ExtendedCamelContext) camelContext).addContextPlugin(XmlRoutesBuilderLoader.class, new CustomXmlRoutesBuilderLoader());
     }
 }
